d7284bac60cce130f7f5950687b39876ed7cffd1
AzizBenSalah <azizbensalahtorro.com@gmail.com>
2024-02-27 23:09:02 +0100

    crud2 + interface2

---
+++ src/main/java/sample/Evenement/Participant.java
@@ -0,0 +1,61 @@
+package sample.Evenement;
+
+public class Participant {
+
+
+    private int id;
+    private String nom;
+    private String prenom;
+    private int age;
+
+
+    public Participant(int id, String nom, String prenom, int age) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Participant{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
